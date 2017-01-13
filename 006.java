6266c95c0309801bd2b853b7fee00fe1e29ff6d9
CodeWithJava <yhan14@stevens.edu>
2017-01-12 23:20:20 -0500

    Seventh Round

---
+++ 006.java
@@ -0,0 +1,42 @@
+public class Solution
+{
+	public String convert(String s, int numRows)
+	{
+		if(s == null || s.length() == 0 || numRows < 2)
+			return s;
+
+		int step = 2 * (numRows - 1);
+		StringBuilder sb = new StringBuilder();
+
+		for(int i = 0;i < numRows;i++)
+		{
+			if(i == 0 || i == numRows - 1)
+			{
+				for(int j = i;j < s.length();j += step)
+					sb.append(s.charAt(j));
+			}
+			else
+			{
+				int j = i;
+				boolean flag = true;
+
+				int step1 = 2 * (numRows - i - 1);
+				int step2 = step - step1;
+
+				while(j < s.length())
+				{
+					sb.append(s.charAt(j));
+
+					if(flag)
+						j += step1;
+					else
+						j += step2;
+
+					flag = !flag;
+				}
+			}
+		}
+
+		return sb.toString();
+	}
+}
