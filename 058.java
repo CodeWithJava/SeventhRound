fe0ec7cfb9ac7b7fcf73acb3dcc6f8585d6131c1
CodeWithJava <yhan14@stevens.edu>
2017-01-14 01:02:57 -0500

    Seventh Round

---
+++ 058.java
@@ -0,0 +1,29 @@
+public class Solution
+{
+	public int lengthOfLastWord(String s)
+	{
+		if(s == null || s.length() == 0)
+			return 0;
+
+		int result = 0;
+		boolean flag = false;
+
+		for(int i = s.length() - 1;i >= 0;i--)
+		{
+			char x = s.charAt(i);
+
+			if(x >= 'a' && x <= 'z' || x >= 'A' && x <= 'Z')
+			{
+				result++;
+				flag = true;
+			}
+			else
+			{
+				if(flag)
+					return result;
+			}
+		}
+
+		return result;
+	}
+}
