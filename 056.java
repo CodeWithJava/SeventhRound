162a9c456badb47822b246f6ff70f54303165d3e
CodeWithJava <yhan14@stevens.edu>
2017-01-13 22:44:05 -0500

    Seventh Round

---
+++ 056.java
@@ -0,0 +1,38 @@
+public class Solution
+{
+	public List<Interval> merge(List<Interval> intervals)
+	{
+		if(intervals == null || intervals.size() == 0)
+			return intervals;
+
+		List<Interval> result = new ArrayList<>();
+
+		Collections.sort(intervals, new IntervalComparator());
+
+		Interval prev = intervals.get(0);
+
+		for(int i = 1;i < intervals.size();i++)
+		{
+			Interval curr = intervals.get(i);
+
+			if(prev. end < curr.start)
+			{
+				result.add(prev);
+				prev = curr;
+			}
+			else
+				prev = new Interval(prev.start, Math.max(prev.end, curr.end));
+		}
+
+		result.add(prev);
+
+		return result;
+	}
+}
+class IntervalComparator implements Comparator<Interval>
+{
+	public int compare(Interval i1, Interval i2)
+	{
+		return i1.start - i2.start;
+	}
+}
