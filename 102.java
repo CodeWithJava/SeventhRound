366f77411e68436e4b0181ce4d32d80004e84084
CodeWithJava <yhan14@stevens.edu>
2017-01-16 16:49:40 -0500

    Seventh Round

---
+++ 102.java
@@ -0,0 +1,35 @@
+public class Solution
+{
+	public List<List<Integer>> levelOrder(TreeNode root)
+	{
+		List<List<Integer>> result = new ArrayList<>();
+
+		if(root == null)
+			return result;
+
+		Queue<TreeNode> q = new LinkedList<>();
+		q.offer(root);
+
+		while(!q.isEmpty())
+		{
+			List<Integer> t = new ArrayList<>();
+			int size = q.size();
+
+			for(int i = 0;i < size;i++)
+			{
+				TreeNode x = q.poll();
+				t.add(x.val);
+
+				if(x.left != null)
+					q.offer(x.left);
+
+				if(x.right != null)
+					q.offer(x.right);
+			}
+
+			result.add(new ArrayList<>(t));
+		}
+
+		return result;
+	}
+}
