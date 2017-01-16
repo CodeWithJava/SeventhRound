2fd3b5905d44522cd3211ed35560965dca6e7b0d
CodeWithJava <yhan14@stevens.edu>
2017-01-16 13:30:20 -0500

    Seventh Round

---
+++ 082.java
@@ -0,0 +1,27 @@
+public class Solution
+{
+	public ListNode deleteDuplicates(ListNode head)
+	{
+		if(head == null || head.next == null)
+			return head;
+
+		ListNode dummyNode = new ListNode(0);
+		dummyNode.next = head;
+		ListNode p = dummyNode;
+
+		while(p.next != null && p.next.next != null)
+		{
+			if(p.next.val == p.next.next.val)
+			{
+				int v = p.next.val;
+
+				while(p.next != null && p.next.val == v)
+					p.next = p.next.next;
+			}
+			else
+				p = p.next;
+		}
+
+		return dummyNode.next;
+	}
+}
