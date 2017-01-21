f11f5035103e5b16bf963599618ae67930b3d1a5
CodeWithJava <yhan14@stevens.edu>
2017-01-21 18:31:43 -0500

    Seventh Round

---
+++ 127.java
@@ -0,0 +1,58 @@
+public class Solution
+{
+	public int ladderLength(String beginWord, String endWord, List<String> wordList)
+	{
+		if(wordList == null || wordList.size() == 0)
+			return 0;
+
+		Queue<Node> q = new LinkedList<>();
+		q.offer(new Node(beginWord, 1));
+
+		wordList.add(endWord);
+
+		while(!q.isEmpty())
+		{
+			Node x = q.poll();
+			String word = x.word;
+
+			if(word.equals(endWord))
+				return x.step;
+
+			char [] alphabets = word.toCharArray();
+
+			for(int i = 0;i < alphabets.length;i++)
+			{
+				char t = alphabets[i];
+
+				for(char c = 'a';c <= 'z';c++)
+				{
+					if(alphabets[i] != c)
+						alphabets[i] = c;
+
+					String newWord = new String(alphabets);
+
+					if(wordList.contains(newWord))
+					{
+						q.offer(new Node(newWord, x.step + 1));
+						wordList.remove(newWord);
+					}
+				}
+
+				alphabets[i] = t;
+			}
+		}
+
+		return 0;
+	}
+}
+class Node
+{
+	int step;
+	String word;
+
+	Node(String word, int step)
+	{
+		this.word = word;
+		this.step = step;
+	}
+}
