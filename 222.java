51d6df8df420b4799fe8e7e67e4428ec82eaba38
CodeWithJava <yhan14@stevens.edu>
2017-01-23 19:35:20 -0500

    Seventh Round

---
+++ 222.java
@@ -0,0 +1,42 @@
+public class Solution
+{
+	public int countNodes(TreeNode root)
+	{
+		if(root == null)
+			return 0;
+
+		int l = getL(root);
+		int r = getR(root);
+
+		if(l == r)
+			return (1 << (l - 1)) - 1;
+
+		return countNodes(root.left) + countNodes(root.right) + 1;
+	}
+
+	private int getL(TreeNode node)
+	{
+		int height = 0;
+
+		while(node != null)
+		{
+			height++;
+			node = node.left;
+		}
+
+		return height;
+	}
+
+	private int getR(TreeNode node)
+	{
+		int height = 0;
+
+		while(node != null)
+		{
+			height++;
+			node = node.right;
+		}
+
+		return height;
+	}
+}
