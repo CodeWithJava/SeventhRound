dca31f698bcebcbdf3b37649db71ed5cbf5ee720
CodeWithJava <yhan14@stevens.edu>
2017-01-25 12:39:58 -0500

    Seventh Round

---
+++ 277.java
@@ -0,0 +1,28 @@
+public class Solution extends Relation
+{
+	public int findCelebrity(int n)
+	{
+		if(n < 0)
+			return -1;
+
+		int l = 0;
+		int r = n - 1;
+
+		while(l < r)
+		{
+			if(knows(l, r))
+				l++;
+			else
+				r--;
+		}
+
+		for(int i = 0;i < n;i++)
+		{
+			if(i != l)
+				if(knows(l, i) || !knows(i, l))
+					return -1;
+		}
+
+		return l;
+	}
+}
