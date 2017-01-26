db53b3168c26289f1e53a5e024e02b20fca8e268
CodeWithJava <yhan14@stevens.edu>
2017-01-26 15:35:48 -0500

    Seventh Round

---
+++ 243.java
@@ -0,0 +1,26 @@
+public class Solution
+{
+	public int shortestDistance(String [] words, String word1, String word2)
+	{
+		if(words == null || words.length == 0)
+			return -1;
+
+		int p = -1;
+		int q = -1;
+
+		int min = Integer.MAX_VALUE;
+
+		for(int i = 0;i < words.length;i++)
+		{
+			if(words[i].equals(word1))
+				p = i;
+			else if(words[i].equals(word2))
+				q = i;
+
+			if(p != -1 && q != -1)
+				min = Math.min(min, Math.abs(p - q));
+		}
+
+		return min;
+	}
+}
