de8026032ca97bb995c753ec767ad7fb14b63e5f
CodeWithJava <yhan14@stevens.edu>
2017-02-04 10:05:09 -0500

    Seventh Round

---
+++ 084.java
@@ -0,0 +1,33 @@
+public class Solution
+{
+	public int largestRectangleArea(int [] heights)
+	{
+		if(heights == null || heights.length == 0)
+			return 0;
+
+		int max = 0;
+
+		Stack<Integer> stack = new Stack<>();
+
+		int i = 0;
+
+		while(i < heights.length)
+		{
+			if(stack.isEmpty() || heights[stack.peek()] <= heights[i])
+				stack.push(i++);
+			else
+			{
+				int x = stack.pop();
+				max = Math.max(max, heights[x] * (stack.isEmpty() ? i:i - 1 - stack.peek()));
+			}
+		}
+
+		while(!stack.isEmpty())
+		{
+			int x = stack.pop();
+			max = Math.max(max, heights[x] * (stack.isEmpty() ? i:i - 1 - stack.peek()));
+		}
+
+		return max;
+	}
+}
