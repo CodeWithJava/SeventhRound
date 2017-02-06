236792c87a8ffa3d2d4a854f016ac34642bbfd61
CodeWithJava <yhan14@stevens.edu>
2017-02-05 20:32:29 -0500

    Seventh Round

---
+++ 120.java
@@ -0,0 +1,24 @@
+public class Solution
+{
+	public int minimumTotal(List<List<Integer>> triangle)
+	{
+		if(triangle == null || triangle.size() == 0)
+			throw new IllegalArgumentException("Input is invalid");
+
+		if(triangle.size() == 1)
+			return triangle.get(0).get(0);
+
+		int size = triangle.size();
+
+		int [] dp = new int [size];
+
+		for(int i = 0;i < size;i++)
+			dp[i] = triangle.get(size - 1).get(i);
+
+		for(int i = size - 2;i >= 0;i--)
+			for(int j = 0;j < triangle.get(i).size();j++)
+				dp[j] = Math.min(dp[j], dp[j + 1]) + triangle.get(i).get(j);
+
+		return dp[0];
+	}
+}
