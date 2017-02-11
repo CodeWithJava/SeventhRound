e6369d0d890e18a57b06e419f2f6888c5d360b59
CodeWithJava <yhan14@stevens.edu>
2017-02-11 15:26:38 -0500

    Seventh Round

---
+++ 352.java
@@ -0,0 +1,49 @@
+public class SummaryRanges
+{
+	TreeSet<Interval> set;
+	/** Initialize your data structure here. */
+	SummaryRanges()
+	{
+		set = new TreeSet<>(new IntervalComparator());
+	}
+
+	public void addNum(int val)
+	{
+		Interval newInterval = new Interval(val, val);
+
+		Interval floor = set.floor(newInterval);
+
+		if(floor != null)
+		{
+			if(floor.end >= newInterval.end)
+				return;
+			else if(floor.end + 1 == newInterval.start)
+			{
+				newInterval.start = floor.start;
+				set.remove(floor);
+			}
+		}
+
+		Interval higher = set.higher(newInterval);
+
+		if(higher != null && newInterval.end + 1 == higher.start)
+		{
+			newInterval.end = higher.end;
+			set.remove(higher);
+		}
+
+		set.add(newInterval);
+	}
+
+	public List<Interval> getIntervals()
+	{
+		return Arrays.asList(set.toArray(new Interval [0]));
+	}
+}
+class IntervalComparator implements Comparator<Interval>
+{
+	public int compare(Interval i1, Interval i2)
+	{
+		return i1.start - i2.start;
+	}
+}
