ca42c233c1d7dc9616e1aca055c412f0218c9ad7
CodeWithJava <yhan14@stevens.edu>
2017-02-12 15:07:12 -0500

    Seventh Round

--- 361.java
+++ 361.java
@@ -1,88 +1,44 @@
-public class Twitter
+public class Solution
 {
-	Map<Integer, List<Integer>> tweet;
-	Map<Integer, List<Integer>> follow;
-	List<Integer> tweetList;
-
-	/** Initialize your data structure here. */
-	public Twitter()
+	public int maxKilledEnemies(char [][] grid)
 	{
-		tweet = new HashMap<>();
-		follow = new HashMap<>();
-		tweetList = new ArrayList<>();
-	}
-	
-	/** Compose a new tweet. */
-	public void postTweet(int userId, int tweetId)
-	{
-		List<Integer> t = new ArrayList<>();
+		if(grid == null || grid.length == 0 || grid[0].length == 0)
+			return 0;
 
-		if(tweet.containsKey(userId))
-			t = tweet.get(userId);
+		int r = grid.length;
+		int c = grid[0].length;
 
-		t.add(tweetId);
-		tweet.put(userId, t);
-		tweetList.add(tweetId);
-	}
-	
-	/** Retrieve the 10 most recent tweet ids in the user's news feed. Each item in the news feed must be posted by users who the user followed or by the user herself. Tweets must be ordered from most recent to least recent. */
-	public List<Integer> getNewsFeed(int userId)
-	{
-		List<Integer> list = new ArrayList<>();
+		int result = 0;
+		int rowHits = 0;
+		int [] colHits = new int [c];
 
-		if(tweet.containsKey(userId))
-			list = tweet.get(userId);
-
-		List<Integer> all = new ArrayList<>(list);
-		List<Integer> followees = new ArrayList<>();
-
-		if(follow.containsKey(userId))
-			followees = follow.get(userId);
-
-		for(int i = 0;i < followees.size();i++)
-			if(tweet.containsKey(followees.get(i)))
-				all.addAll(tweet.get(followees.get(i)));
-
-		Set<Integer> set = new HashSet<>(all);
-		List<Integer> feed = new ArrayList<>();
-
-		int k = 0;
-
-		for(int i = tweetList.size() - 1;i >= 0;i--)
+		for(int i = 0;i < r;i++)
 		{
-			if(k == 10)
-				break;
-
-			int tweetId = tweetList.get(i);
-
-			if(set.contains(tweetId))
+			for(int j = 0;j < c;j++)
 			{
-				feed.add(tweetId);
-				k++;
+				if(j == 0 || grid[i][j - 1] == 'W')
+				{
+					rowHits = 0;
+
+					for(int k = j;k < c && grid[i][k] != 'W';k++)
+						if(grid[i][k] == 'E')
+							rowHits++;
+				}
+
+				if(i == 0 || grid[i - 1][j] == 'W')
+				{
+					colHits[j] = 0;
+
+					for(int k = i;k < r && grid[k][j] != 'W';k++)
+						if(grid[k][j] == 'E')
+							colHits[j]++;
+				}
+
+				if(grid[i][j] == '0')
+					result = Math.max(result, rowHits + colHits[j]);
 			}
 		}
 
-		return feed;
-	}
-	
-	/** Follower follows a followee. If the operation is invalid, it should be a no-op. */
-	public void follow(int followerId, int followeeId)
-	{
-		List<Integer> t = new ArrayList<>();
-
-		if(follow.containsKey(followerId))
-			t = follow.get(followerId);
-
-		if(!t.contains(followeeId))
-			t.add(followeeId);
-
-		follow.put(followerId, t);
-	}
-	
-	/** Follower unfollows a followee. If the operation is invalid, it should be a no-op. */
-	public void unfollow(int followerId, int followeeId)
-	{
-		if(follow.containsKey(followerId))
-			follow.get(followerId).remove((Integer)followeeId);
+		return result;
 	}
 }
