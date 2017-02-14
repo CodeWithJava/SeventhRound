4dabbfe247f51139a81dc796b8d6777c33d97797
CodeWithJava <yhan14@stevens.edu>
2017-02-13 22:54:50 -0500

    Seventh Round

---
+++ 504.java
@@ -0,0 +1,31 @@
+// Solution One
+public class Solution
+{
+	public String convertToBase7(int num)
+	{
+		return Integer.toString(num, 7);
+	}
+}
+
+// Solution Two
+public class Solution
+{
+	public String convertToBase7(int num)
+	{
+		if(num == 0)
+			return "0";
+
+		String result = "";
+
+		boolean isNegative = num < 0;
+        	num = Math.abs(num);
+
+		while(num != 0)
+		{
+			result = num % 7 + result;
+			num /= 7;
+		}
+
+		return isNegative ? "-" + result:result;
+	}
+}
