815a7f4668a61bdc70b0fa1dfd92566c3fd9d32d
CodeWithJava <yhan14@stevens.edu>
2017-02-20 13:59:59 -0500

    Seventh Round

---
+++ 419.java
@@ -0,0 +1,26 @@
+public class Solution
+{
+	public int countBattleships(char [][] board)
+	{
+		if(board == null || board.length == 0 || board[0].length == 0)
+			return 0;
+
+		int r = board.length;
+		int c = board[0].length;
+
+		int count = 0;
+
+		for(int i = 0;i < r;i++)
+		{
+			for(int j = 0;j < c;j++)
+			{
+				if(board[i][j] == '.') continue;
+				if(i > 0 && board[i - 1][j] == 'X') continue;
+				if(j > 0 && board[i][j - 1] == 'X') continue;
+				count++;
+			}
+		}
+
+		return count;
+	}
+}
