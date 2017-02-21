9cdea957a39b7fcb9b9a880ea11683d8092be774
CodeWithJava <yhan14@stevens.edu>
2017-02-20 21:56:21 -0500

    Seventh Round

--- 494.java
+++ 494.java
@@ -1,3 +1,4 @@
+// Solution One
 public class Solution
 {
 	int count;
@@ -29,5 +30,44 @@ public class Solution
 			traversal(nums, i + 1, target - nums[i]);
 			traversal(nums, i + 1, target + nums[i]);
 		}
+	}
+}
+
+// Solution Two
+public class Solution
+{
+	public int findTargetSumWays(int [] nums, int S)
+	{
+		if(nums == null || nums.length == 0)
+			return 0;
+
+		int sum = 0;
+
+		for(int x: nums)
+			sum += x;
+
+		if(s > sum || s < -sum)
+			return 0;
+
+		int [] dp = new int [sum * 2 + 1];
+		dp[sum] = 1;
+
+		for(int i = 0;i < nums.length;i++)
+		{
+			int [] next = new int [sum * 2 + 1];
+
+			for(int j = 0;j < sum * 2 + 1;j++)
+			{
+				if(dp[j]!= 0)
+				{
+					next[j + nums[i]] += dp[j];
+					next[j - nums[i]] += dp[j];
+				}
+			}
+
+			dp = next;
+		}
+
+		return dp[sum + s];
 	}
 }
