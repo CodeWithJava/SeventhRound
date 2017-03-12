63b0834d2a62a1f0e8caac302a8a239ec0bae538
CodeWithJava <yhan14@stevens.edu>
2017-03-12 18:42:52 -0400

    Seventh Round

--- 530.java
+++ 530.java
@@ -1,15 +1,16 @@
 // BST Solution
 public class Solution
 {
-	int minDifference = Integer.MAX_VALUE;
+	int minDifference = Integer.MAX_VALUE:
 	TreeNode prev;
 
 	public int getMinimumDifference(TreeNode root)
 	{
 		if(root == null)
-			return Integer.MAX_VALUE;
+			return minDifference;
 
 		traversal(root);
+
 		return minDifference;
 	}
 
@@ -22,6 +23,7 @@ public class Solution
 
 		if(prev != null)
 			minDifference = Math.min(minDifference, node.val - prev.val);
+
 		prev = node;
 
 		traversal(node.right);
@@ -31,13 +33,13 @@ public class Solution
 // BT Solution
 public class Solution
 {
-	Set<Integer> set = new TreeSet<>();
 	int minDifference = Integer.MAX_VALUE;
+	TreeSet<Integer> set = new TreeSet<>();
 
 	public int getMinimumDifference(TreeNode root)
 	{
 		if(root == null)
-			return Integer.MAX_VALUE;
+			return minDifference;
 
 		traversal(root);
 
@@ -53,8 +55,9 @@ public class Solution
 		{
 			if(set.floor(node.val) != null)
 				minDifference = Math.min(minDifference, node.val - set.floor(node.val));
+
 			if(set.ceiling(node.val) != null)
-				min = Math.min(minDifference, set.ceiling(node.val) - node.val);
+				minDifference = Math.min(minDifference, set.ceiling(node.val) - node.val);
 		}
 
 		set.add(node.val);
