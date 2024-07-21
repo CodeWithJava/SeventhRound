46678aa252eb32268ec67c786744fe0e06ba1f83
CodeWithJava <yabinhan0902@gmail.com>
2024-07-20 23:53:25 -0700

    Update 040.java

--- 040.java
+++ 040.java
@@ -1,3 +1,4 @@
+// Solution One
 public class Solution
 {
 	public List<List<Integer>> combinationSum2(int [] candidates, int target)
@@ -28,6 +29,13 @@ public class Solution
 		{
 			for(int i = start;i < candidates.length;i++)
 			{
+				// Handle the case (duplicate elements in the array candidates)
+				// candidates = [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,
+				// 1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,
+				// 1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,
+				// 1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1]
+				// target = 30
+				
 				if(start < i && candidates[i - 1] == candidates[i])
 					continue;
 				if(target < candidates[i])
@@ -39,4 +47,42 @@ public class Solution
 			}
 		}
 	}
+}
+
+// Solution Two
+class Solution {
+    public List<List<Integer>> combinationSum2(int [] candidates, int target) {
+        List<List<Integer>> result = new ArrayList<>();
+
+        if (candidates == null || candidates.length == 0) {
+            return result;
+        }
+
+        Arrays.sort(candidates);
+
+        traversal(candidates, 0, target, new ArrayList<>(), result);
+
+        return result;
+    }
+
+    private void traversal(int [] candidates, int start, int target, List<Integer> t, List<List<Integer>> result) {
+        if (target == 0) {
+            result.add(new ArrayList<>(t));
+        } else {
+            for (int i = start;i < candidates.length;i++) {
+                // Handle the duplicate result
+		if (start < i && candidates[i - 1] == candidates[i]) {
+                    continue;
+                }
+
+                if (candidates[i] > target) {
+                    return;
+                }
+
+                t.add(candidates[i]);
+                traversal(candidates, i + 1, target - candidates[i], t, result);
+                t.remove(t.size() - 1);
+            }
+        }
+    }
 }
