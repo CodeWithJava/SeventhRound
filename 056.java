2e1319a791f0e85c8bba32973ec25a4080113941
CodeWithJava <yabinhan0902@gmail.com>
2024-07-21 19:43:30 -0700

    Update 056.java

--- 056.java
+++ 056.java
@@ -39,3 +39,33 @@ class IntervalComparator implements Comparator<Interval>
 		return i1.start - i2.start;
 	}
 }
+
+// Newer Version
+// For the current input and output type
+class Solution {
+    public int [][] merge(int [][] intervals) {
+        if (intervals == null || intervals.length == 0) {
+            return new int [0][0];
+        }
+
+        Arrays.sort(intervals, (i1, i2) -> Integer.compare(i1[0], i2[0]));
+
+        List<int []> t = new ArrayList<>();
+        int [] prev = intervals[0];
+
+        for (int i = 1;i < intervals.length;i++){
+            int [] curr = intervals[i];
+
+            if (prev[1] < curr[0]) {
+                t.add(prev);
+                prev = curr;
+            } else {
+                prev = new int [] {prev[0], Math.max(prev[1], curr[1])};
+            }
+        }
+
+        t.add(prev);
+
+        return t.toArray(new int [t.size()][]);
+    }
+}
