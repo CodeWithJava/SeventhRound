433af6b0e8f65225112362ffc85f8db3ff81d62b
CodeWithJava <yabinhan0902@gmail.com>
2024-07-23 22:22:05 -0700

    Update 117.java

--- 117.java
+++ 117.java
@@ -1,3 +1,47 @@
+// The solution for the current version
+class Solution {
+    public Node connect(Node root) {
+        Node lastHead = root;
+        Node lastCurrent = null;
+        Node currentHead = null;
+        Node current = null;
+
+        while (lastHead != null) {
+            lastCurrent = lastHead;
+
+            while (lastCurrent != null) {
+                if (lastCurrent.left != null) {
+                    if (currentHead == null) {
+                        currentHead = lastCurrent.left;
+                        current = currentHead;
+                    } else {
+                        current.next = lastCurrent.left;
+                        current = current.next;
+                    }
+                }
+
+                if (lastCurrent.right != null) {
+                    if (currentHead == null) {
+                        currentHead = lastCurrent.right;
+                        current = currentHead;
+                    } else {
+                        current.next = lastCurrent.right;
+                        current = current.next;
+                    }
+                }
+
+                lastCurrent = lastCurrent.next;
+            }
+
+            lastHead = currentHead;
+            currentHead = null;
+        }
+
+        return root;
+    }
+}
+
+// The solution for the older version
 public class Solution
 {
 	public void connect(TreeLinkNode root)
