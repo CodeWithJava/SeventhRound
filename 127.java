eac8a00f9c457d209cae1cc4b1981a2e50ecc6a5
CodeWithJava <yabinhan0902@gmail.com>
2024-07-25 14:23:09 -0700

    Update 127.java

--- 127.java
+++ 127.java
@@ -1,59 +1,54 @@
-public class Solution
-{
-	public int ladderLength(String beginWord, String endWord, List<String> wordList)
-	{
-		if(wordList == null || wordList.size() == 0)
-			return 0;
+class Solution {
+    public int ladderLength(String beginWord, String endWord, List<String> wordList) {
+        if (wordList == null || wordList.size() == 0) {
+            return 0;
+        }
 
-		Queue<Node> q = new LinkedList<>();
-		q.offer(new Node(beginWord, 1));
+        Set<String> set = new HashSet<>();
+        set.addAll(wordList);
 
-		Set<String> set = new HashSet<>();
-        	set.addAll(wordList);
+        Queue<Node> q = new LinkedList<>();
+        q.offer(new Node(beginWord, 1));
 
-		while(!q.isEmpty())
-		{
-			Node x = q.poll();
-			String word = x.word;
+        while (!q.isEmpty()) {
+            Node x = q.poll();
+            String word = x.word;
 
-			if(word.equals(endWord))
-				return x.step;
+            if (word.equals(endWord)) {
+                return x.step;
+            }
 
-			char [] alphabets = word.toCharArray();
+            char [] alphabets = word.toCharArray();
 
-			for(int i = 0;i < alphabets.length;i++)
-			{
-				char t = alphabets[i];
+            for (int i = 0;i < alphabets.length;i++) {
+                char t = alphabets[i];
 
-				for(char c = 'a';c <= 'z';c++)
-				{
-					if(alphabets[i] != c)
-						alphabets[i] = c;
+                for (char c = 'a';c <= 'z';c++) {
+                    if (alphabets[i] != c) {
+                        alphabets[i] = c;
 
-					String newWord = new String(alphabets);
+                        String newWord = new String(alphabets);
 
-					if(set.contains(newWord))
-					{
-						q.offer(new Node(newWord, x.step + 1));
-						set.remove(newWord);
-					}
-				}
+                        if (set.contains(newWord)) {
+                            q.offer(new Node(newWord, x.step + 1));
+                            set.remove(newWord);
+                        }
+                    }
+                }
 
-				alphabets[i] = t;
-			}
-		}
+                alphabets[i] = t;
+            }
+        }
 
-		return 0;
-	}
+        return 0;
+    }
 }
-class Node
-{
-	int step;
-	String word;
+class Node {
+    int step;
+    String word;
 
-	Node(String word, int step)
-	{
-		this.word = word;
-		this.step = step;
-	}
+    Node(String word, int step) {
+        this.word = word;
+        this.step = step;
+    }
 }
