ed1d843d2429f62f6df0946592b9dbb041c0a444
CodeWithJava <yabinhan0902@gmail.com>
2024-07-28 20:42:43 -0700

    Update 152.java

--- 152.java
+++ 152.java
@@ -1,23 +1,21 @@
-public class Solution
-{
-	public int maxProduct(int [] nums)
-	{
-		if(nums == null || nums.length == 0)
-			throw new IllegalArgumentException("Input is invalid");
+class Solution {
+    public int maxProduct(int [] nums) {
+        if (nums == null || nums.length == 0) {
+            throw new IllegalArgumentException("Input is invalid");
+        }
 
-		int globalMax = nums[0];
-		int localMax = nums[0];
-		int localMin = nums[0];
+        double globalMax = nums[0];
+        double localMax = nums[0];
+        double localMin = nums[0];
 
-		for(int i = 1;i < nums.length;i++)
-		{
-			int t = localMax;
-			localMax = Math.max(nums[i], Math.max(localMax * nums[i], localMin * nums[i]));
-			localMin = Math.min(nums[i], Math.min(t * nums[i], localMin * nums[i]));
+        for (int i = 1;i < nums.length;i++) {
+            double t = localMax;
+            localMax = Math.max(nums[i], Math.max(localMax * nums[i], localMin * nums[i]));
+            localMin = Math.min(nums[i], Math.min(t * nums[i], localMin * nums[i]));
 
-			globalMax = Math.max(globalMax, localMax);
-		}
+            globalMax = Math.max(globalMax, localMax);
+        }
 
-		return globalMax;
-	}
+        return (int)globalMax;
+    }
 }
