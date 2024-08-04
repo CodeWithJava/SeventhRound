5e9e9f70448991b7235563c998ec960018523a9b
CodeWithJava <yabinhan0902@gmail.com>
2024-08-03 21:51:11 -0700

    Update 313.java

--- 313.java
+++ 313.java
@@ -1,29 +1,30 @@
-public class Solution
-{
-	public int nthSuperUglyNumber(int n, int [] primes)
-	{
-		if(n < 1 || primes == null || primes.length == 0)
-			return 0;
+class Solution {
+    public int nthSuperUglyNumber(int n, int [] primes) {
+        if (n < 1 || primes == null || primes.length == 0) {
+            return 0;
+        }
 
-		List<Integer> list = new ArrayList<>();
-		list.add(1);
+        List<Double> list = new ArrayList<>();
+        list.add(1.0);
 
-		int [] idx = new int [primes.length];
+        int [] idx = new int [primes.length];
 
-		while(list.size() < n)
-		{
-			int min = Integer.MAX_VALUE;
+        while (list.size() < n) {
+            double min = Double.POSITIVE_INFINITY;
 
-			for(int i = 0;i < primes.length;i++)
-				min = Math.min(min, primes[i] * list.get(idx[i]));
+            for (int i = 0;i < primes.length;i++) {
+                min = Math.min(min, list.get(idx[i]) * primes[i]);
+            }
 
-			list.add(min);
+            list.add(min);
 
-			for(int i = 0;i < primes.length;i++)
-				if(primes[i] * list.get(idx[i]) == min)
-					idx[i]++;
-		}
+            for (int i = 0;i < primes.length;i++) {
+                if (list.get(idx[i]) * primes[i] == min) {
+                    idx[i]++;
+                }
+            }
+        }
 
-		return list.get(n - 1);
-	}
+        return (int) list.get(n - 1).doubleValue();
+    }
 }
