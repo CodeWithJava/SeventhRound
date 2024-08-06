df4ad8f26cb84c1b7b141132604a7f2f1a3787f9
CodeWithJava <yabinhan0902@gmail.com>
2024-08-05 19:07:19 -0700

    Update 501.java

--- 501.java
+++ 501.java
@@ -22,7 +22,7 @@ public class Solution
 		for(int i = 0;i < list.size();i++)
 		    result[i] = list.get(i);
 
-        return result;
+        	return result;
 	}
 
 	private void traversal(TreeNode node)
